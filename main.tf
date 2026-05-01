22b4fb641a291d633f67f5978bc2f7b2063e2d64
Imran <imrankhanmohammed130@gmail.com>
2026-05-01 16:02:03 -0500

    learning terraformhc

--- main.tf
+++ main.tf
@@ -19,5 +19,5 @@ resource "aws_route53_record" "frontend" {
 }
 
 variable "components" {
-  default = ["frontend", "postgresql","portfolio-service","anayltics-service", "auth-service"]
+  default = ["frontend", "postgresql","portfolio-service","anayltics-service", ]
 }
