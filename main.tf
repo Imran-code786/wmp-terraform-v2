cf64fe3d43133b8a79ad0e241691a90dc03a3dd9
Imran <imrankhanmohammed130@gmail.com>
2026-06-09 23:14:56 -0500

    learninbg tersds

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "instance" {
-  count         = 5
-  ami           = "ami-0220d79f3f480ecf5"
-  instance_type = "t3.small"
+  count                  = 5
+  ami                    = "ami-0220d79f3f480ecf5"
+  instance_type          = "t3.small"
   vpc_security_group_ids = ["sg-04692f74cd3fa91ac"]
 
   tags = {
@@ -10,14 +10,14 @@ resource "aws_instance" "instance" {
 }
 
 resource "aws_route53_record" "frontend" {
-  count = 5
+  count   = 5
   zone_id = "Z02346282FFZR4IVNTZCB"
   name    = "${var.components[count.index]}-dev"
   type    = "A"
   ttl     = 30
-  records  = [aws_instance.instance[count.index].private_ip]
+  records = [aws_instance.instance[count.index].private_ip]
 }
 
 variable "components" {
-  default = ["frontend", "postgresql","portfolio-service", "auth-service","anayltics-service" ]
+  default = ["frontend", "postgresql", "portfolio-service", "auth-service", "anayltics-service"]
 }
